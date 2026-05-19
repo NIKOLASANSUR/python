7802b6c27432816eea5bc1f5972bb8d0a6593b5a
Nikolas Ansur Proti Soares <nikolas.ansur@gmail.com>
2026-05-19 11:47:44 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def questao1():
+    return render_template("questao1.html", nome="Janaína")
+
+@app.route('/questao2')
+def questao2():
+    dados = {"nome": "Janaína", "idade": 18}
+    return render_template("questao2.html", dados=dados)
+
+@app.route('/questao3')
+def questao3():
+    usuario = {"nome": "Ana", "email": "ana@email.com"}
+    return render_template("questao3.html", usuario=usuario)
 
 if __name__ == '__main__':
     app.run(debug=True)
